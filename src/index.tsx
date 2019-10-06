dae776a34a88828832096d3ebed9f5509fbbacd4
Marcus Correa <mrc.correa@gmail.com>
2019-10-06 03:13:58 -0300

    Removed header to test.

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import * as React from 'react'
 import { render } from 'react-dom'
 import { observer } from 'mobx-react'
 import './media/styles/main.scss'
-import { Header } from './components/header'
+// import { Header } from './components/header'
 import ScheduleContainer from './app/Schedule/ScheduleContainer'
 
 @observer
@@ -10,7 +10,7 @@ class App extends React.Component<{}, {}> {
   render() {
     return (
       <div>
-        <Header />
+        {/* <Header /> */}
         <ScheduleContainer />
       </div>
     )
